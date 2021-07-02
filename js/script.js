f8809894bd55d2e04c7c78bacf8c1d165eb891c6
NXMX GXLDXX <nxmxgoldxx@live.com>
2021-07-01 20:20:18 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// console.log("hi");
+
+var mainTitle = document.querySelector("h1");
+mainTitle.innerText = "Yay, Javascript!!!";
+var body = document.querySelector("p");
+body.style.backgroundColor = "#FEf2F1";
+
+console.log(mainTitle);
